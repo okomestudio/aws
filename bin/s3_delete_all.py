9d9cc76c7021bddf3eeb2d98ca56176d7d34805f
Taro Sato <okomestudio@gmail.com>
2015-04-08 11:29:01 -0700

    do concurrent delete

--- bin/s3_delete_all.py
+++ bin/s3_delete_all.py
@@ -2,25 +2,80 @@
 # -*- coding: utf-8 -*-
 """Delete multiple keys.
 
+Examples
+--------
+
+  $ s3_delete_keys.py somebucket a b
+
+will delete all keys prefixed by 'a' and 'b' in bucket somebucket.
+
 """
 import gevent.monkey
 gevent.monkey.patch_all()
 
 from argparse import ArgumentParser
+import logging
 
-import boto
 import gevent
+import gevent.pool
 
 from aws.s3 import s3
 
 
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+log.addHandler(ch)
+
+
 def main(args):
-    bucket = boto.connect_s3().get_bucket(args.bucket)
-    for prefix in args.key_prefix:
-        for i, key in enumerate(bucket.list(prefix=prefix, marker=args.marker)):
-            if i > 1000:
-                break
-            print key.name
+    pool_size = 10
+    group = gevent.pool.Pool(len(args.key_prefix))
+
+    def task_group(group_id, bucket_name, prefix):
+        pool = gevent.pool.Pool(10)
+
+        def delete_keys(bucket_name, key_names):
+            if len(key_names) == 0:
+                return
+            log.info(u'deleting keys from {} to {} in {}'
+                     .format(key_names[0], key_names[-1], bucket_name))
+            retry = 3
+            for i in xrange(retry):
+                try:
+                    s3.delete_keys(bucket_name, key_names)
+                except Exception, e:
+                    log.warn(u'error: {}, retry {}'.format(repr(e), i + 1))
+                    continue
+                else:
+                    break
+            else:
+                raise
+
+        def log_qsize(wait):
+            while True:
+                log.info(u'{}: number of concurrent delete tasks = {}'
+                         .format(group_id, pool_size - pool.free_count()))
+                gevent.sleep(wait)
+
+        g_log_qsize = gevent.spawn(log_qsize, 15)
+
+        key_names = []
+        for i, key in enumerate(s3.list(bucket_name, prefix=prefix, marker=args.marker)):
+            key_names.append(key.name)
+            if len(key_names) < 1000:
+                continue
+            pool.spawn(delete_keys, bucket_name, key_names)
+            key_names = []
+        pool.spawn(delete_keys, bucket_name, key_names)
+        pool.join()
+        g_log_qsize.kill()
+        g_log_qsize.join()
+
+    bucket_name = args.bucket
+    for group_id, prefix in enumerate(args.key_prefix):
+        group.spawn(task_group, group_id, bucket_name, prefix)
+    group.join()
 
 
 if __name__ == '__main__':
